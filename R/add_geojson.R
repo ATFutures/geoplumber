840c55610339ea3c4805397ab96116f5d6518c12
Layik Hama <layik.hama@gmail.com>
2018-09-18 12:49:35 +0100

    gp erase doc, do not reimport components per #32, thanks to @mpadge

--- R/add_geojson.R
+++ R/add_geojson.R
@@ -42,11 +42,15 @@ gp_add_geojson <- function(endpoint = "/api/data"){
   # Import new component
   # Above 'export default'
   export.index <- grep("export default", welcome)
-  welcome <- c(welcome[1:export.index - 1],
-               # import GeoJSONComponent from '/components/GeoJSONComponent.jsx';
-               paste0("import ", component.name, " from './", component.path, "';"),
-               welcome[export.index:length(welcome)]
-  )
+  # check for duplicate
+  component.name.added <- grepl(paste0("import ", component.name), welcome)
+  if(!any(component.name.added)) {
+    welcome <- c(welcome[1:export.index - 1],
+                 # import GeoJSONComponent from '/components/GeoJSONComponent.jsx';
+                 paste0("import ", component.name, " from './", component.path, "';"),
+                 welcome[export.index:length(welcome)]
+    )
+  }
   # find end map component tag
   map.end.index <- grep(pattern = "</Map>", x = welcome)
   # TODO: more checks as file could be corrupt
