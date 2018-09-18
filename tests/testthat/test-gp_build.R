954a5f2d14390fe480278fa7a4297c5b35bfc7bc
Layik Hama <layik.hama@gmail.com>
2018-09-18 12:24:16 +0100

    rm gp_ from R files, per #33

--- tests/testthat/test-gp_build.R
+++ tests/testthat/test-gp_build.R
@@ -3,5 +3,5 @@ context("test-gp_build")
 test_that("build fails for non geoplumber path", {
   # expect_message takes character vector.
   expect_message(gp_build(), "Geoplumber failed to identify a package.json in working directory: ",
-                                    getwd(), "Run gp_create() to create a geoplumber app.")
+                 getwd(), "\nRun gp_create() to create a geoplumber app.")
 })
